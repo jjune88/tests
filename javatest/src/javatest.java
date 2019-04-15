b6e00f075688550f08c85292b28defb892afd7bd
jjune88 <sping1988@gmail.com>
2019-04-15 15:14:40 +0900

    java

--- javatest/src/javatest.java
+++ javatest/src/javatest.java
@@ -1,11 +1,8 @@
-package javatest;
-
-public class tes {
+public class javatest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!!");
-		
 	}
 
 }
